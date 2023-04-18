d92980120237cd5c76f90da5f634fc91483bfca7
seefalert <36908690+seefalert@users.noreply.github.com>
2023-04-18 14:05:01 +0300

    v1.0

---
+++ python_bot/bot.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config_data.config import load_config, Config
+from handlers import other_handlers, user_handlers
+
+
+async def main() -> None:
+    config: Config = load_config('.env')
+
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
